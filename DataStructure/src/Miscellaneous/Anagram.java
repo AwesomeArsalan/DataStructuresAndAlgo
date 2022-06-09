5886c118e7cb29a6dd0b5c84453939f65d6fbfe8
Arsalan Mahmood <mdarsalan95@gmail.com>
2022-06-09 21:56:02 +0530

    Added new programs

---
+++ DataStructure/src/Miscellaneous/Anagram.java
@@ -0,0 +1,36 @@
+package Miscellaneous;
+
+import java.util.Arrays;
+
+public class Anagram {
+
+	public static void main(String[] args) {
+		String input1 = "silent";
+		String input2 = "listen";
+		boolean res = areAnagram(input1, input2);
+		if(res) {
+			System.out.println(input1 + " and " + input2 + " are Anagram.");
+		} else {
+			System.out.println(input1 + " and " + input2 + " are not Anagram");
+		}
+	}
+
+	public static boolean areAnagram(String input1, String input2) {
+		boolean areAnagram = true;
+		if (null == input1 || null == input2 || input1.length() != input2.length()) {
+			return false;
+		}
+		char[] str1 = input1.toCharArray();
+		char[] str2 = input2.toCharArray();
+		Arrays.sort(str1);
+		Arrays.sort(str2);
+		for (int i = 0; i < str1.length; i++) {
+			if(str1[i] != str2[i]) {
+				areAnagram = false;
+				break;
+			}
+		}
+		return areAnagram;
+	}
+
+}
--- DataStructure/src/sorting/BubbleSort.java
+++ DataStructure/src/sorting/BubbleSort.java
@@ -11,16 +11,16 @@ public class BubbleSort {
 
 	private static void bubbleSort(int[] arr) {
 		for (int i = 0; i < arr.length - 1; i++) {
-			boolean sorted = true;
+			boolean alreadySorted = true;
 			for (int k = 0; k < arr.length - 1; k++) {
 				if (arr[k] > arr[k + 1]) {
 					int temp = arr[k];
 					arr[k] = arr[k + 1];
 					arr[k + 1] = temp;
-					sorted = false;
+					alreadySorted = false;
 				}
 			}
-			if (sorted) {
+			if (alreadySorted) {
 				break;
 			}
 		}
